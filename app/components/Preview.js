74356b5232062de5ea81d6390436ba9773c6a63b
Anton Kindblad <anton@k-mit.se>
2015-05-07 00:32:24 +0200

    copy button now works and copies correct html to pasteboard

--- app/components/Preview.js
+++ app/components/Preview.js
@@ -17,22 +17,22 @@ var Preview = React.createClass({
         },
         rawFooter                : function () {
             return '<div style="font-family: \'Arial\'; font-size: 12px; color: #000; line-height: 14px">' +
-                   this.state.info.user_name + '<br>' +
-                   this.state.info.user_position + '<br>' +
-                   this.state.info.user_department + '<br>' +
-                   '<br>' +
-                   '-------------------------------------<br>' +
-                   '<br>' +
-                   '<a href="' + this.state.logo.properties.link + '" target="_blank"><img border="0" width="' + this.state.logo.properties.size.width + '" height="' + this.state.logo.properties.size.height + '" title="' + this.state.logo.properties.title + '" alt="' + this.state.logo.properties.title + '"  src="' + this.state.logo.properties.url + '" /></a><br>' +
-                   '<br>' +
-                   this.state.company + '<br>' +
-                   this.state.address.user_street + '<br>' +
-                   this.state.address.user_city + ', ' + this.state.address.user_state + ' ' + this.state.address.user_zip + '<br>' +
-                   this.state.address.user_country + '<br>' +
-                   '<br>' +
-                   (this.state.contacts.user_phone.length > 0 ? 'Phone:' : '') + ' ' + this.state.contacts.user_phone + '<br>' +
-                   (this.state.contacts.user_mobile.length > 0 ? 'Mobile:' : '') + ' ' + this.state.contacts.user_mobile + '<br>' +
-                   '<a href="mailto:' + this.state.contacts.user_email + '" style="font-family: \'Arial\';font-size  : 12px;color     : #0046ad;line-height: 14px">' + this.state.contacts.user_email + '</a>' + '<br>' +
+                   this.state.info.user_name + '<br>\n' +
+                   this.state.info.user_position + '<br>\n' +
+                   this.state.info.user_department + '<br>\n' +
+                   '<br>\n' +
+                   '-------------------------------------<br>\n' +
+                   '<br>\n' +
+                   '<a href="' + this.state.logo.properties.link + '" target="_blank"><img border="0" width="' + this.state.logo.properties.size.width + '" height="' + this.state.logo.properties.size.height + '" title="' + this.state.logo.properties.title + '" alt="' + this.state.logo.properties.title + '"  src="' + this.state.logo.properties.url + '" /></a><br>\n' +
+                   '<br>\n' +
+                   this.state.company + '<br>\n' +
+                   this.state.address.user_street + '<br>\n' +
+                   this.state.address.user_city + ', ' + this.state.address.user_state + ' ' + this.state.address.user_zip + '<br>\n' +
+                   this.state.address.user_country + '<br>\n' +
+                   '<br>\n' +
+                   (this.state.contacts.user_phone.length > 0 ? 'Phone:' : '') + ' ' + this.state.contacts.user_phone + '<br>\n' +
+                   (this.state.contacts.user_mobile.length > 0 ? 'Mobile:' : '') + ' ' + this.state.contacts.user_mobile + '<br>\n' +
+                   '<a href="mailto:' + this.state.contacts.user_email + '" style="font-family: \'Arial\';font-size  : 12px;color     : #0046ad;line-height: 14px">' + this.state.contacts.user_email + '</a>' + '<br>\n' +
                    '</div>';
         },
         render                   : function () {
@@ -48,7 +48,7 @@ var Preview = React.createClass({
                     <div>
                         <div>
                             <p>Click the button to copy some text</p>
-                            <ReactZeroClipboard text="Hello, world!">
+                            <ReactZeroClipboard text={this.rawFooter}>
                                 <button>Copy</button>
                             </ReactZeroClipboard>
                         </div>
