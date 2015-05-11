50379883f49b26d55354f261dabbe3bed5a655fd
Anton Kindblad <anton@k-mit.se>
2015-05-11 14:19:45 +0200

    adding popover when copy to clipboard

--- app/components/Preview.js
+++ app/components/Preview.js
@@ -6,7 +6,8 @@
 var React = require('react');
 var ZeroClipboard = require('zeroclipboard');
 var ReactZeroClipboard = require('react-zeroclipboard');
-
+var Popover = require('react-bootstrap/lib/Popover');
+var OverlayTrigger= require('react-bootstrap/lib/OverlayTrigger');
 var Preview = React.createClass({
         getInitialState          : function () {
             return this.props.initUserObj;
@@ -51,7 +52,10 @@ var Preview = React.createClass({
                         <div>
                             <p>Press the button to copy the footer to the clipboard and then paste it into your mail clients field for the footer.</p>
                             <ReactZeroClipboard text={this.rawFooter}>
-                                <button className="largerbutton">Copy footer to clipboard</button>
+                                <OverlayTrigger container={this} trigger='click' placement='top' overlay={<Popover title='Footer generated'>The HTML-code to show your footer has been copied to your clipboard. Paste the footer into your email client</Popover>}>
+                                    <button className="btn btn-primary btn-lg">Copy footer to clipboard</button>
+                                </OverlayTrigger>
+
                             </ReactZeroClipboard>
                         </div>
                     </div>
